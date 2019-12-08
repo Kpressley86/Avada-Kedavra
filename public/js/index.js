fed766bb0cdf1bd0dcb448665ba03b8d1f3689a4
Kelly Pressley <kellypressley86@gmail.com>
2019-12-08 13:54:25 -0700

    fixed home page links and started a table on happy hour page

--- public/js/index.js
+++ public/js/index.js
@@ -97,3 +97,52 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+
+// Array for beers //
+let beers = [
+  ["Codename: Superfan", "IPA", "6.5%" ,"Colorado", "$5.00"],
+  ["Intergalactic Juice Hunter", "Double IPA", "8.0%", "Colorado", "$6.00"],
+  ["Jetman Jimmy", "Golden Ale", "4.5%", "Colorado", "$4.50"],
+  ["NOOB", "Pale Ale", "6.0%", "Colorado", "$5.00"],
+  ["Vincent Van Couch", "Sour", "4.8%", "Colorado", "$5.00"],
+  { name: "1337 H4x0r", type: "TRIPPLE IPA", abv: "10.0%", location: "Colorado", cost: "$9.00" },
+  { name: "Admiral Abyss", type: "Chocolate Stout", abv: "7.0%", location: "Colorado", cost: "$6.00" },
+  { name: "Alpha Twins", type: "Double IPA", abv: "8.25%", location: "Colorado", cost: "$7.00" },
+  { name: "Haole Bartender", type: "Sour", abv: "6.0%", location: "Ccolorado", cost: "$5.00" },
+  { name: "Fresh Haze", type: "IPA", abv: "6.5%", location: "Oregon", cost: "$5.00" },
+  { name: "Mirror pond", type: "Pale Ale", abv: "5.0%", location: "Oregon", cost: "$5.00" },
+  { name: "Black Butte", type: "Porter", abv: "5.2%", location: "Oregon", cost: "$5.00" },
+  { name: "Obsidian", type: "Stout", abv: "6.4%", location: "Oregon", cost: "$5.00" },
+  { name: "Oatis", type: "Stout", abv: "7.0%", location: "Oregon", cost: "$6.00" },
+  { name: "Dawn of the Red", type: "Red IPA", abv: "7.0%", location: "Oregon", cost: "$6.00" },
+  { name: "Pacific Rain", type: "Pale Ale", abv: "5.4%", location: "Oregon", cost: "$5.00" },
+  { name: "RPM", type: "IPA", abv: "6.5%", location: "Oregon", cost: "$6.00" },
+  { name: "Skunk Ape", type: "IRA", abv: "6.0%", location: "Oregon", cost: "$5.00" },
+  { name: "Armored Fist", type: "IDA", abv: "9.0%", location: "Oregon", cost: "$7.00" },
+  { name: "Moon Juice", type: "IPA", abv: "7.3%", location: "Arizona", cost: "$6.00" },
+  { name: "Juicy Jack", type: "Hazy IPA", abv: "6.5%", location: "Arizona", cost: "$5.00" },
+  { name: "1920", type: "Lager", abv: "4.7%", location: "Arizona", cost: "$4.00" },
+  { name: "Coors Light", type: "Light Beer", abv: "4.2%", location: "Colorado", cost: "$4.00" },
+  { name: "Bud Light", type: "Light Beer", abv: "5.0%", location: "USA", cost: "$4.00" },
+  { name: "Nebula", type: "Stout", abv: "6.0%", location: "Oregon", cost: "$5.00" },
+  { name: "Blocktoberfest", type: "Marzen", abv: "5.5%", location: "Oregon", cost: "$5.00" },
+  { name: "Dark Matter", type: "Chocolate Porter", abv: "5.0%", location: "Oregon", cost: "$5.00" },
+  { name: "Drive Me To Firenze", type: "Pilsner", abv: "4.7%", location: "Oregon", cost: "$5.00" },
+  { name: "Imagine", type: "Imperial Stout", abv: "13.5%", location: "Oregon", cost: "$9.00" },
+  { name: "Oud Bruin", type: "Brown Ale", abv: "6.5%", location: "Oregon", cost: "$6.00" },
+  { name: "Specular Relfections", type: "Belgian Double", abv: "6.5%", location: "Oregon", cost: "$6.00" },
+  { name: "Summer Amusement", type: "Saison", abv: "4.5%", location: "Oregon", cost: "$4.00" },
+  { name: "Wandelpad", type: "Blonde", abv: "6.3%", location: "Oregon", cost: "$5.50" },
+  { name: "Crush", type: "Sour", abv: "5.0%", location: "Oregon", cost: "$5.00" },
+  { name: "Apocalypse", type: "IPA", abv: "6.8%", location: "Oregon", cost: "$6.50" }
+],
+  table = document.getElementById("table");
+
+for (let i = 0; i < beers.length; i++) {
+  var newRow = table.insertRow(table.length);
+  for (let d = 0; d < beers[1].length; d++) {
+      var cell = newRow.insertCell(j);
+      cell.innerHTML = beers[i][j];
+  }
+}
