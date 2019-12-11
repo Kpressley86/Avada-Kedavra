b112121e5c2bea3481fce09d57456df7530324cc
Kelly Pressley <kellypressley86@gmail.com>
2019-12-10 17:38:34 -0700

    still working on form data and storing in database

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,24 @@
 $(document).ready(function () {
   // Get references to page elements
-  var $submitBtn = $("#submit")
-  var $exampleText = $("#example-text");
-  var $exampleDescription = $("#example-description");
-  var $exampleList = $("#example-list");
+  var $day = $("#day");
+  var $submitBtn = $("#submit");
+  var $firstName = $("#firstName");
+  var $lastName = $("#lastName");
+  var $email = $("#email");
 
   // The API object contains methods for each kind of request we'll make
   var API = {
-    saveExample: function (example) {
+    saveExample: function(newBeerReservation) {
       return $.ajax({
         headers: {
           "Content-Type": "application/json"
         },
         type: "POST",
-        url: "api/examples",
-        data: JSON.stringify(example)
+        url: "api/reservations",
+        data: JSON.stringify(newBeerReservation)
       });
     },
-    getExamples: function () {
+    getExamples: function() {
       return $.ajax({
         url: "api/examples",
         type: "GET"
@@ -34,7 +35,7 @@ $(document).ready(function () {
   // refreshExamples gets new examples from the db and repopulates the list
   var refreshExamples = function () {
     API.getExamples().then(function (data) {
-      var $examples = data.map(function (example) {
+      var $examples = data.map(function (newBeerReservation) {
         var $a = $("<a>")
           .text(example.text)
           .attr("href", "/example/" + example.id);
@@ -65,39 +66,44 @@ $(document).ready(function () {
   var handleFormSubmit = function (event) {
     event.preventDefault();
 
-    var example = {
-      text: $exampleText.val().trim(),
-      description: $exampleDescription.val().trim()
+    var newBeerReservation = {
+      Day: $day.val().trim(),
+      FirstName: $firstName.val().trim(),
+      LastName: $lastName.val().trim(),
+      EmailAddress: $email.val().trim(),
     };
 
-    if (!(example.text && example.description)) {
-      alert("You must enter an example text and description!");
+
+    if (!($email.text)) {
+      alert("You must fill out all feilds first!");
       return;
     }
 
-    API.saveExample(example).then(function () {
+    API.saveExample(newBeerReservation).then(function () {
       refreshExamples();
     });
 
-    $exampleText.val("");
-    $exampleDescription.val("");
+    $day.val("");
+    $firstName.val("");
+    $lastName.val("");
+    $email.val("");
   };
 
   // handleDeleteBtnClick is called when an example's delete button is clicked
   // Remove the example from the db and refresh the list
-  var handleDeleteBtnClick = function () {
-    var idToDelete = $(this)
-      .parent()
-      .attr("data-id");
+  // var handleDeleteBtnClick = function () {
+  //   var idToDelete = $(this)
+  //     .parent()
+  //     .attr("data-id");
 
-    API.deleteExample(idToDelete).then(function () {
-      refreshExamples();
-    });
-  };
+  //   API.deleteExample(idToDelete).then(function () {
+  //     refreshExamples();
+  //   });
+  // };
 
   // Add event listeners to the submit and delete buttons
   $submitBtn.on("click", handleFormSubmit);
-  $exampleList.on("click", ".delete", handleDeleteBtnClick);
+  // $exampleList.on("click", ".delete", handleDeleteBtnClick);
 
   // Array for beers //
   var beer1 = [
@@ -107,42 +113,42 @@ $(document).ready(function () {
     "NOOB", "Pale Ale", "6.0%", "Colorado", "$5.00",
     "Vincent Van Couch", "Sour", "4.8%", "Colorado", "$5.00"];
 
-  let beer2 = [
+  var beer2 = [
     "1337 H4x0r", "TRIPPLE IPA", "10.0%", "Colorado", "$9.00",
     "Admiral Abyss", "Chocolate Stout", "7.0%", "Colorado", "$6.00",
     "Alpha Twins", "Double IPA", "8.25%", "Colorado", "$7.00",
     "Haole Bartender", "Sour", "6.0%", "Ccolorado", "$5.00",
     "Fresh Haze", "IPA", "6.5%", "Oregon", "$5.00"];
 
-  let beer3 = [
+  var beer3 = [
     "Mirror pond", "Pale Ale", "5.0%", "Oregon", "$5.00",
     "Black Butte", "Porter", "5.2%", "Oregon", "$5.00",
     "Obsidian", "Stout", "6.4%", "Oregon", "$5.00",
     "Oatis", "Stout", "7.0%", "Oregon", "$6.00",
     "Dawn of the Red", "Red IPA", "7.0%", "Oregon", "$6.00"];
 
-  let beer4 = [
+  var beer4 = [
     "Pacific Rain", "Pale Ale", "5.4%", "Oregon", "$5.00",
     "RPM", "IPA", "6.5%", "Oregon", "$6.00",
     "Skunk Ape", "IRA", "6.0%", "Oregon", "$5.00",
     "Armored Fist", "IDA", "9.0%", "Oregon", "$7.00",
     "Moon Juice", "IPA", "7.3%", "Arizona", "$6.00"];
 
-  let beer5 = [
+  var beer5 = [
     "Juicy Jack", "Hazy IPA", "6.5%", "Arizona", "$5.00",
     "1920", "Lager", "4.7%", "Arizona", "$4.00",
     "Coors Light", "Light Beer", "4.2%", "Colorado", "$4.00",
     "Bud Light", "Light Beer", "5.0%", "USA", "$4.00",
     "Nebula", "Stout", "6.0%", "Oregon", "$5.00"];
 
-  let beer6 = [
+  var beer6 = [
     "Blocktoberfest", "Marzen", "5.5%", "Oregon", "$5.00",
     "Dark Matter", "Chocolate Porter", "5.0%", "Oregon", "$5.00",
     "Drive Me To Firenze", "Pilsner", "4.7%", "Oregon", "$5.00",
     "Imagine", "Imperial Stout", "13.5%", "Oregon", "$9.00",
     "Oud Bruin", "Brown Ale", "6.5%", "Oregon", "$6.00"];
 
-  let beer7 = [
+  var beer7 = [
     "Specular Relfections", "Belgian Double", "6.5%", "Oregon", "$6.00",
     "Summer Amusement", "Saison", "4.5%", "Oregon", "$4.00",
     "Wandelpad", "Blonde", "6.3%", "Oregon", "$5.50",
@@ -271,39 +277,21 @@ $(document).ready(function () {
       customerLastName: $("#lastName").val().trim(),
       customerEmailAddress: $("#email").val().trim(),
     };
-var email = $("#email").val().trim();
-var day = $("#day").val().trim();
-var firstName = $("#firstName").val().trim();
+    var Email = $("#email").val().trim();
+    var day = $("#day").val().trim();
+    var firstName = $("#firstName").val().trim();
 
     console.log(newBeerReservation);
 
-    //Creating the API calls 
-    $.post("/api/reservations", newBeerReservation,
-      function (data) {
-        if (data) {
-          alert("you have been booked. be on the look out for an email");
-        }
-        else {
-          alert("nope. try a different day")
-        }
-
-        //clear the user's input after submitting 
-        $("#day").val("");
-        $("#firstName").val("");
-        $("#lastName").val("");
-        $("#email").val("");
-      });
-
-    // SEND E-MAIL ALERT // 
-    Email.send({
-      Host: "https://avada-kedavra2.herokuapp.com/",
-      Username: `${firstName}`,
-      Password: "password",
-      To: `${email}`,
-      From: "Avada-Kedavra@theScout.com",
-      Subject: "Your Reservation at The Scout",
-      Body: `You now have a reservation for ${day}`
-    }).then(message => alert(message));
-
+    // SEND E-MAIL ALERT
+    // Email.send({
+    //   Host: "https://avada-kedavra2.herokuapp.com/",
+    //   Username: `${firstName}`,
+    //   Password: "password",
+    //   To: `${Email}`,
+    //   From: "Avada-Kedavra@theScout.com",
+    //   Subject: "Your Reservation at The Scout",
+    //   Body: `You now have a reservation for ${day}`
+    // }).then(message => alert(message));
   });
 });
